fc271442cb4c43055b99e6e3f9ba173ac29e93b5
Brenner Costa <brennergpcosta@gmail.com>
2021-08-02 22:35:44 -0300

    Basic functions

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const Post = require("./models/post")
 const mongoose = require("mongoose")
 
+
 const app = express();
 //rDP1vbZsWmGZsM3D meanStackAdmin
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb+srv://meanStackAdmin:rDP1vbZsWmGZsM3D@meanstackcluster.pxcqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(
+    'mongodb+srv://meanStackAdmin:rDP1vbZsWmGZsM3D@meanstackcluster.pxcqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+     { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to the database')
   })
@@ -34,27 +37,29 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  post.save();
-  res.status(201).json({
-    message: "Post added successfully"
-  });
+  post.save().then(result => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: result._id
+    });
+  })
 });
 
 app.get("/api/posts", (req, res, next) => {
-  Post.find().then(documents => {
+  Post.find().then(posts => {
     res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-    });
-  });
+      message: 'Fetch was successful',
+      posts: posts
+    })
+  })
 });
 
 app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result)
+    res.status(200).json({ message: "Post has been deleted"})
+  })
+})
 
 module.exports = app;
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,38 +1,57 @@
-import { Injectable } from "@angular/core";
-import { Post } from "./post.model";
-import { Subject } from "rxjs"
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
-
+@Injectable({ providedIn: 'root' })
 export class PostsService {
-  private posts: Post[] = []
-  private postUpdated = new Subject<Post[]>()
+  private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
 
   constructor(private http: HttpClient) {}
 
-  getPostUpdatedListener(){
-    return this.postUpdated.asObservable()
+  getPostUpdatedListener() {
+    return this.postUpdated.asObservable();
   }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').
-      subscribe((postsData) => {
-        this.posts = postsData.posts
-        this.postUpdated.next([...this.posts])
-      })
+    this.http
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        })
+      }))
+      .subscribe((tranformedData) => {
+        this.posts = tranformedData;
+        this.postUpdated.next([...this.posts]);
+      });
   }
 
   addPost(title: string, content: string) {
     const post: Post = { id: '', title: title, content: content };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
-      .subscribe(responseData => {
-        console.log(responseData.message);
+      .post<{ message: string, postId: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        post.id = responseData.postId
+        console.log(post)
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
       });
   }
 
-
+  deletePost(id: string) {
+    this.http
+      .delete(`http://localhost:3000/api/posts/${id}`)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== id)
+        this.posts = updatedPosts
+        this.postUpdated.next([...this.posts])
+      })
+  }
 }
